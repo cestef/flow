aedadbb7d9ad8684778b29d66315112e41773bec
cstef <colin@petit-suisse.fr>
2023-08-19 21:29:25 +0200

    Add a 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+export default function Page404() {
+	return (
+		<div className="flex flex-col items-center justify-center w-screen h-screen shadow-sm">
+			<div className="flex flex-col items-center justify-center">
+				<h1 className="text-6xl font-bold">404</h1>
+				<p className="text-gray-500 text-xl pt-2">
+					We couldn't find the page you were looking for.
+				</p>
+			</div>
+		</div>
+	);
+}
