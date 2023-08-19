9ab7a5dcbbccdf4cda9f8b38131c334857ec23c8
cstef <colin@petit-suisse.fr>
2023-08-19 19:45:28 +0200

    Update head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<title>Flow</title>
+				<meta
+					name="description"
+					content="Flow is a collaborative diagramming tool"
+				/>
+				<meta name="theme-color" content="#004F30" />
+				<meta name="color-scheme" content="dark light" />
+				<meta name="og:title" content="Flow" />
+				<meta
+					name="og:description"
+					content="Flow is a collaborative diagramming tool"
+				/>
+				<meta name="og:image" content="/icon.png" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
