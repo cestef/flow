cd16b7d41ba9d7deaef9082a757f16d01e95ed5b
cstef <colin@petit-suisse.fr>
2023-08-24 23:27:55 +0200

    Fix kbd layout

--- src/components/ui/keyboard.tsx
+++ src/components/ui/keyboard.tsx
@@ -7,13 +7,11 @@ export default function Keyboard({
 }) {
 	return (
 		<kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-			<span className="text-xs">
-				{modifiers.map((modifier) => (
-					<span key={modifier} className="text-muted-foreground">
-						{modifier}
-					</span>
-				))}
-			</span>
+			{modifiers.map((modifier) => (
+				<span key={modifier} className="ml-1 text-muted-foreground">
+					{modifier}
+				</span>
+			))}
 			{keys.map((key) => (
 				<span key={key} className="text-muted-foreground">
 					{key}
