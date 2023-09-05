573fc95b43ecf8f4453012b1595b685bda4f5ecf
cstef <colin@petit-suisse.fr>
2023-09-05 18:49:33 +0200

    Update branding

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,14 +8,14 @@ export default function Document() {
 					name="description"
 					content="Flow is a collaborative diagramming tool"
 				/>
-				<meta name="theme-color" content="#004F30" />
+				<meta name="theme-color" content="#020817" />
 				<meta name="color-scheme" content="dark light" />
 				<meta name="og:title" content="Flow" />
 				<meta
 					name="og:description"
 					content="Flow is a collaborative diagramming tool"
 				/>
-				<meta name="og:image" content="/icon.png" />
+				<meta name="og:image" content="/banner.png" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<body>
