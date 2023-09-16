b690e0f4f8fb8c095a17f929fd1430ab600aebc6
cstef <colin@petit-suisse.fr>
2023-09-16 21:15:30 +0200

    Remove canvas panel

--- src/components/flow/panels/index.tsx
+++ src/components/flow/panels/index.tsx
@@ -1,24 +1,6 @@
 import { useStore } from "@/lib/store";
-import { Panel } from "reactflow";
-import CanvasPanel from "./canvas";
-import { cn } from "@/lib/utils";
 
 export default function Panels() {
 	const canvasPanelOpen = useStore((state) => state.canvasPanel);
-	return (
-		<>
-			<Panel
-				position="bottom-left"
-				className={cn("transition-all duration-300 ease-in-out", {
-					"z-[4]": !canvasPanelOpen,
-					"z-[6]": canvasPanelOpen,
-					"transform -translate-x-[calc(100%-3.5rem)] translate-y-[calc(100%-3.5rem)]":
-						!canvasPanelOpen,
-				})}
-				onContextMenu={(e) => e.preventDefault()}
-			>
-				<CanvasPanel />
-			</Panel>
-		</>
-	);
+	return <></>;
 }
