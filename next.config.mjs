e0b055a45649392a0d005f1893de0e126d2c88c3
cstef <colin@petit-suisse.fr>
2023-09-16 20:59:38 +0200

    Guess who committed .env ?

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
 		WS_URL: process.env.WS_URL,
         PLUV_WS_URL: process.env.PLUV_WS_URL,
         PLUV_AUTH_URL: process.env.PLUV_AUTH_URL,
-		GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
+		UMAMI_WEBSITE_ID: process.env.UMAMI_WEBSITE_ID,
 		VERSION: pack.default.version,
 	},
     images: {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import getConfig from "next/config";
 
+const { publicRuntimeConfig } = getConfig();
+const { UMAMI_WEBSITE_ID } = publicRuntimeConfig;
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				{UMAMI_WEBSITE_ID && (
+					<script
+						async
+						src="https://analytics.umami.is/script.js"
+						data-website-id={UMAMI_WEBSITE_ID}
+					/>
+				)}
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
